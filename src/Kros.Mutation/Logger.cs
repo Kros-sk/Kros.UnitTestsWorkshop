c97d37d0ca2ab4e01e1567beb77d6f45dc1cd355
Milan Martiniak <mino.martiniak@gmail.com>
2023-10-06 16:27:05 +0200

    comments another tests

---
+++ src/Kros.Mutation/Logger.cs
@@ -0,0 +1,14 @@
+// namespace Kros.Mutation;
+
+// public class Logger : ILogger
+// {
+//     public string LastMessage { get; private set; } = string.Empty;
+
+//     public static ILogger Default { get; } = new Logger();
+
+//     public void Log(string message)
+//     {
+//         Console.WriteLine(message);
+//         LastMessage = message;
+//     }
+// }
