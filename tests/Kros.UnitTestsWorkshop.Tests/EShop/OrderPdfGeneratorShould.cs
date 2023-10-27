8f0d4823e1c339ca1662fdabc601c9d6cab85293
Milan Martiniak <mino.martiniak@gmail.com>
2023-10-27 14:13:37 +0200

    fix verify

--- tests/Kros.UnitTestsWorkshop.Tests/EShop/OrderPdfGeneratorShould.cs
+++ tests/Kros.UnitTestsWorkshop.Tests/EShop/OrderPdfGeneratorShould.cs
@@ -1,61 +1,74 @@
-namespace Kros.UnitTestsWorkshop.Tests.EShop;
+//using Kros.UnitTestsWorkshop.EShop;
+//using QuestPDF.Infrastructure;
+//using System.Runtime.CompilerServices;
 
-public class OrderPdfGeneratorShould
-{
-    // [Fact]
-    // public void GeneratePdf()
-    // {
-    //     var order = CreateOrder();
-    //     var generator = new OrderPdfGenerator();
-    //     IDocument pdf = generator.Generate(order);
-    // }
+//namespace Kros.UnitTestsWorkshop.Tests.EShop;
 
-    // private static Order CreateOrder()
-    // {
-    //     var client = new Client
-    //     {
-    //         FirstName = "John",
-    //         LastName = "Doe",
-    //         Email = "john.doe@example.com",
-    //         Address = "123 Main St, Example City"
-    //     };
+//[UsesVerify]
+//public class OrderPdfGeneratorShould
+//{
+//    [ModuleInitializer]
+//    internal static void Init()
+//    {
+//        VerifyQuestPdf.Initialize();
+//    }
 
-    //     var orderItems = new List<OrderItem>
-    //     {
-    //         new() {
-    //             Product = "Laptop",
-    //             Amount = 1,
-    //             UnitPrice = 1000.00M,
-    //             Discount = 0,
-    //             VatRate = 0.20M,
-    //             TotalPrice = 1000.00M,
-    //             TotalPriceWithVat = 1200.00M
-    //         },
-    //         new() {
-    //             Product = "Mouse",
-    //             Amount = 2,
-    //             UnitPrice = 20.00M,
-    //             Discount = 0,
-    //             VatRate = 0.20M,
-    //             TotalPrice = 40.00M,
-    //             TotalPriceWithVat = 48.00M
-    //         }
-    //     };
-    //     var order = new Order
-    //     {
-    //         Number = "ORD123456",
-    //         Client = client,
-    //         OrderDate = DateTime.Now,
-    //         EstimatedDeliveryDate = DateTime.Now.AddDays(5),
-    //         PaymentMethod = "Credit Card",
-    //         Items = orderItems,
-    //         Discount = 0,
-    //         TotalPrice = 1040.00M,
-    //         VatBase = 0.20M,
-    //         VatAmount = 208.00M,
-    //         TotalPriceWithVat = 1248.00M
-    //     };
+//    [Fact]
+//    public Task GeneratePdf()
+//    {
+//        var order = CreateOrder();
+//        var generator = new OrderPdfGenerator();
+//        IDocument pdf = generator.Generate(order);
 
-    //     return order;
-    // }
-}
+//        // ???
+//    }
+
+//    private static Order CreateOrder()
+//    {
+//        var client = new Client
+//        {
+//            FirstName = "John",
+//            LastName = "Doe",
+//            Email = "john.doe@example.com",
+//            Address = "123 Main St, Example City"
+//        };
+
+//        var orderItems = new List<OrderItem>
+//         {
+//             new() {
+//                 Product = "Laptop",
+//                 Amount = 1,
+//                 UnitPrice = 1000.00M,
+//                 Discount = 0,
+//                 VatRate = 0.20M,
+//                 TotalPrice = 1000.00M,
+//                 TotalPriceWithVat = 1200.00M
+//             },
+//             new() {
+//                 Product = "Mouse",
+//                 Amount = 2,
+//                 UnitPrice = 20.00M,
+//                 Discount = 0,
+//                 VatRate = 0.20M,
+//                 TotalPrice = 40.00M,
+//                 TotalPriceWithVat = 48.00M
+//             }
+//         };
+//        var order = new Order
+//        {
+//            Number = "ORD123456",
+//            Client = client,
+//            OrderDate = DateTime.Now,
+//            EstimatedDeliveryDate = DateTime.Now.AddDays(5),
+//            PaymentMethod = "Credit Card",
+//            Items = orderItems,
+//            Discount = 0,
+//            TotalPrice = 1040.00M,
+//            VatBase = 0.20M,
+//            VatAmount = 208.00M,
+//            TotalPriceWithVat = 1248.00M
+//        };
+
+//        return order;
+//    }
+//}
